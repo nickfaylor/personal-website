704c457fab62835939f37a23f7e8b0bba11e5b16
Nick Faylor <nicholasfaylor2003@gmail.com>
2024-07-25 20:02:25 -0700

    fixed navbar redirects

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const projectsHeader = document.querySelector('.projectsHeader');
     const projectCards = document.querySelectorAll('.project-card');
+    const projectsTab = document.getElementById('projects-tab');
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -10,9 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, {
-        threshold: 0.3 // Trigger when 10% of the element is in view
+        threshold: 0.3 // Trigger when 30% of the element is in view
     });
 
     observer.observe(projectsHeader);
     projectCards.forEach(card => observer.observe(card));
+
+    projectsTab.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default anchor behavior
+        projectsHeader.scrollIntoView({ behavior: 'smooth' });
+    });
 });
